01cf03b3f5d8b775f6d60d76e7b2d6082b10ee59
ESy <2462214616@qq.com>
2023-02-26 22:02:21 +0800

    add redis

--- src/test/java/com/ares/ztserve/ZtserveApplicationTests.java
+++ src/test/java/com/ares/ztserve/ZtserveApplicationTests.java
@@ -1,13 +1,19 @@
 package com.ares.ztserve;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class ZtserveApplicationTests {
-
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
     @Test
-    void contextLoads() {
+    void redisTest() {
+        stringRedisTemplate.opsForValue().set("name","卷心菜");
+        String name = (String) stringRedisTemplate.opsForValue().get("name");
+        System.out.println(name); //卷心菜
     }
 
 }
