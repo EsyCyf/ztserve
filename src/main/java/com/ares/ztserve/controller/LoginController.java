b8ff1cd6f34cf04423dfcf674ed4d9cbf89f4cef
ESy <2462214616@qq.com>
2023-02-27 11:22:04 +0800

    修改登录，原appcontroller改为serve

--- src/main/java/com/ares/ztserve/controller/LoginController.java
+++ src/main/java/com/ares/ztserve/controller/LoginController.java
@@ -3,11 +3,14 @@ package com.ares.ztserve.controller;
 import com.ares.ztserve.model.Msg;
 import com.ares.ztserve.model.User;
 import com.ares.ztserve.service.impl.UserServiceImpl;
+import io.swagger.annotations.ApiOperation;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -23,8 +26,9 @@ public class LoginController {
     @Autowired
     private StringRedisTemplate redisTemplate;
 
+    @ApiOperation(value = "登录接口，返回状态码+token，账号密码对应xx_personnel_base.email和user_password")
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginByname(@RequestParam("username") String username, @RequestParam("password") String password) {
+    public Msg loginByname(@RequestParam("username") String username, @RequestParam("password") String password) {
         User user = userService.findUserByName(username);
         System.out.println("密码是："+user.getPassword());
         if (user.getPassword().equals(password)) {
@@ -33,15 +37,17 @@ public class LoginController {
             System.out.println("token: " + token);
             redisTemplate.opsForValue().set(token,username,3600, TimeUnit.SECONDS);
             System.out.println("redis返回："+(String) redisTemplate.opsForValue().get(token));
-            return token;
+            Msg msg = Msg.success("登录成功");
+            msg.add("token",token);
+            return msg;
         }
-        return "登录失败";
+        return Msg.fail("登录失败");
     }
 
-    @RequiresPermissions("99")
+    /*@RequiresPermissions("99")
     @GetMapping("/login1")
     public Msg index(@RequestHeader String token){
         System.out.println("token: " + token);
         return Msg.success("yes");
-    }
+    }*/
 }
