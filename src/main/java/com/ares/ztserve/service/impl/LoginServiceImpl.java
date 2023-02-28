469e02ac28be38bf8c0042fa58f13871f5f86a9e
ESy <2462214616@qq.com>
2023-02-28 15:34:04 +0800

    规范代码

--- src/main/java/com/ares/ztserve/service/impl/LoginServiceImpl.java
+++ src/main/java/com/ares/ztserve/service/impl/LoginServiceImpl.java
@@ -4,7 +4,6 @@ import com.ares.ztserve.model.Msg;
 import com.ares.ztserve.model.User;
 import com.ares.ztserve.service.LoginService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -24,7 +23,11 @@ public class LoginServiceImpl implements LoginService {
     /**
      * MUTI_LOGIN_ALLOWED 是否允许多端登录
      */
-    private final boolean MUTI_LOGIN_ALLOWED = true;
+    private static final boolean MULTI_LOGIN_ALLOWED = true;
+    /**
+     * EXPIRE_TIME 单位秒
+     */
+    private static final long EXPIRE_TIME = 3600;
 
     @Override
     public Msg login(String username, String password) {
@@ -40,18 +43,18 @@ public class LoginServiceImpl implements LoginService {
             /*System.out.println("登录成功：username： " + username + " password: " + password);*/
             String token = UUID.randomUUID().toString().replaceAll("-", "");
             /*System.out.println("token: " + token);*/
-            /*限制多端登录*/
-            if (!MUTI_LOGIN_ALLOWED) {
+            /*限制多端登录，考虑到有可能增加的请求刷新时长问题，暂不使用*/
+            if (!MULTI_LOGIN_ALLOWED) {
                 if (Boolean.TRUE.equals(redisTemplate.hasKey(username))) {
                     String oldToken = redisTemplate.opsForValue().get(username);
                     if (oldToken != null) {
                         redisTemplate.delete(oldToken);
                     }
                 }
+                redisTemplate.opsForValue().set(username, token, EXPIRE_TIME, TimeUnit.SECONDS);
             }
 
-            redisTemplate.opsForValue().set(token, username, 3600, TimeUnit.SECONDS);
-            redisTemplate.opsForValue().set(username, token, 3600, TimeUnit.SECONDS);
+            redisTemplate.opsForValue().set(token, username, EXPIRE_TIME, TimeUnit.SECONDS);
             System.out.println("redis返回username：" + (String) redisTemplate.opsForValue().get(token));
             System.out.println("redis返回token：" + (String) redisTemplate.opsForValue().get(username));
             Msg msg = Msg.success("登录成功");
